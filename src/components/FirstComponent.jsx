55a431dcb20a418b41dca407bab4f971ccd4b7b6
sachinjadhav <sachinjdv7@gmail.com>
2024-11-21 15:46:16 +0530

    first component added

--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Toast from './Toast';
 
 const FirstComponent = () => {
-  return <div>FirstComponent</div>;
+  const [showToast, setShowToast] = useState(false);
+
+  const handleShow = () => {
+    setShowToast((prev) => !prev);
+  };
+  return (
+    <div className="m-6 p-2">
+      <button
+        className="m-2 p-4 bg-blue-400 text-white rounded-lg"
+        onClick={handleShow}
+      >
+        Show Toast Message
+      </button>
+      <span className="m-2 p-2 bg-blue-100 rounded-lg cursor-pointer">⚙️</span>
+      {showToast && <Toast />}
+    </div>
+  );
 };
 
 export default FirstComponent;
---
+++ src/components/Toast.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Toast = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <span className="bg-green-400 p-5 rounded-lg">showing the toast</span>
+    </div>
+  );
+};
+
+export default Toast;
